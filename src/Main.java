821a37e78aa1f498a86be7fff76b802288fcdc3e
SvetlanaV <svetlana.vasilyeva@infopulse.com>
2022-11-27 14:26:20 +0200

    created two additional messages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        System.out.println("QA Automation the Best");
+        System.out.println("our work with Git");
     }
+
 }
